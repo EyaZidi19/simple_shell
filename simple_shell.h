0faa667ed9baa632d5d7705ffa36ebf3a9ff1631
EyaZidi19 <3805@holbertonschool.com>
2021-12-07 08:40:53 -0800

    header file update

--- simple_shell.h
+++ simple_shell.h
@@ -4,6 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * struct envvar - struct to pass environment information
+ * @varname: name of variable
+ * @next: next in list
+ */
+typedef struct envvar
+{
+	char *varname;
+	struct envvar *next;
+} envvar_t;
+
+extern char **environ;
+
+envvar_t *add_env_node(envvar_t **head, char *varname);
 int _putchar(char c);
+
+
+
 void _env(void);
 #endif
