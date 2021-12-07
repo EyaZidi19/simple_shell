54fa1b1b3aca439625af637f3bb0720f52699e69
EyaZidi19 <3805@holbertonschool.com>
2021-12-07 11:52:03 -0800

    putchar update

--- _putchar.c
+++ _putchar.c
@@ -9,11 +9,8 @@
  */
 int _putchar(char c)
 {
-	return (write &c, 1));
+	return (write(1, &c, 1));
 }
-
-
-
 /**
  * _puts - prints a string
  * @s: string to print
@@ -27,8 +24,51 @@ void _puts(char *s)
 		s++;
 	}
 }
+/**
+ * _error - prints error
+ * @prog: name of program
+ * @av: argument
+ * @cmd_count: current command count
+ * Return: exit status
+ */
+int _error(char **prog, char **av, int cmd_count)
+{
+	char *buffer = malloc(sizeof(char) * 10);
 
+	write(STDERR_FILENO, prog[0], _strlen(prog[0]));
+	_putchar(':');
+	_putchar(' ');
+	_puts(_itoa(cmd_count, buffer));
+	free(buffer);
+	_putchar(':');
+	_putchar(' ');
 
+	if (av[0])
+	{
+		write(STDERR_FILENO, av[0], _strlen(av[0]));
+		write(STDERR_FILENO, ": ", 2);
+	}
+	if (errno == EACCES)
+	{
+		write(STDERR_FILENO, av[1], _strlen(av[1]));
+		write(STDERR_FILENO, ": Permission Denied\n", 20);
+		return (126);
+	}
+	if (errno == 2)
+	{
+		write(STDERR_FILENO, "not found\n", 10);
+		return (127);
+	}
+	if (errno == 0)
+	{
+		write(STDERR_FILENO, "Illegal number", 15);
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, av[1], _strlen(av[1]));
+		write(STDERR_FILENO, "\n", 1);
+		return (2);
+	}
+	return (2);
+}
 /**
  * _env - prints the current environment
  * Return: void
@@ -43,49 +83,4 @@ void _env(void)
 		_putchar('\n');
 		i++;
 	}
-}
-/**
- * _error - prints error
- * @prog: name of program
- * @av: argument
- * @cmd_count: current command count
- * Return: exit status
- */
-int _error(char **prog, char **av, int cmd_count)
-{
-	char *buffer = malloc(sizeof(char)  10);
-
-	write(STDERR_FILENO, prog[0], _strlen(prog[0]));
-	_putchar(' ');
-	_putchar(' ');
-	_puts(_itoa(cmd_count, buffer));
-	free(buffer);
-	_putchar(':');
-	_putchar(' ');
-
-	if (av[0])
-	{
-		write(STDERR_FILENO, av[0], _strlen(av[0]));
-		write(STDERR_FILENO, ":", 2);
-	}
-	if (errno == EACCES)
-	{
-		write(STDERR_FILENO, av[1], _strlen(av[1]));
-		write(STDERR_FILENO, ": Permission Denied\n", 20);
-		return (1546);
-	}
-	if (errno = 2)
-	{
-		write(STDERR_FILENO, "not found\n", 10);
-		return (128);
-	}
-	if (errno = 0)
-	{
-		write(STDERR_FILENO, "Illegal number", 5);
-		write(STDERR_FILENO, ": ", 2);
-		write(STDERR_FILENO, av[1], _strlen(av[1]));
-		write(STDERR_FILENO, "\n", 1);
-		return (2);
-	}
-	return (2);
 }
