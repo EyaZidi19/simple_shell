a3311fe37dd09c27d794efd9ff95fce5f8a31354
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2021-12-08 21:52:51 +0100

    main.c

--- main.c
+++ main.c
@@ -17,3 +17,35 @@ int main(int __attribute__((unused))ac, char *argv[])
 	signal(SIGINT, sigintHandler);
 	while (1)
 	{
+        if (isatty(STDIN_FILENO))
+			write(STDERR_FILENO, "$ ", 2);
+		cmd_count++;
+		gl = getline(&lineptr, &n, stdin);
+		if (gl < 0)
+		{
+			if (isatty(STDIN_FILENO))
+				_putchar('\n');
+			free(lineptr);
+			exit(0);
+		}
+		if (word_count(lineptr) == 0)
+			continue;
+		command = split_line(lineptr);
+		free(lineptr);
+		lineptr = NULL;
+		if (_strcmp(command[0], "exit") == 0)
+		{
+			exit_handler(argv, command, cmd_count, exit_status);
+			exit_status = 2;
+			continue;
+		}
+		if (_strcmp(command[0], "env") == 0)
+		{
+			_env();
+			ffree(command);
+			continue;
+		}
+		exit_status = _execute(argv, command, cmd_count);
+	}
+	return (0);
+}
