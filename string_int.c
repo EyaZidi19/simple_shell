87a976936a36a050f005c1f191730ed7647b7b6a
EyaZidi19 <3805@holbertonschool.com>
2021-12-07 17:05:15 -0800

    update

--- string_int.c
+++ string_int.c
@@ -22,13 +22,12 @@ int iscmd(char *filename)
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 1 if s1 is greater,
- * -1 if less than s2, 0 if equal.
+ * Return: 1 if s1 is greater, -1 if less than s2, 0 if equal
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 ! '\0' && *s2 != '\0') && *s1 = *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
