968bd9f0e7b8c3a19a11883100cee4f67057dcd0
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2021-12-08 21:36:17 +0100

    helperfan.c

--- helpersfan.c
+++ helpersfan.c
@@ -35,3 +35,21 @@ char *_getenv(char *name)
 	}
 	return (NULL);
 }
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		s[i] = 0;
+	}
+	return (s);
+}
